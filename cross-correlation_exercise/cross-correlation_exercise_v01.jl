b903dc2aa045e31f0ba8e4d9436fb077eb233720
kurama <kura.okubo@gmail.com>
2019-01-16 17:08:11 -0500

    update crosscorrelation

--- cross-correlation_exercise/cross-correlation_exercise_v01.jl
+++ cross-correlation_exercise/cross-correlation_exercise_v01.jl
@@ -23,7 +23,7 @@ t_init = 20	# initial time of signal at sensor 1
 #------------------------------------------------#
 
 # Making time series (pseudo inputs)
-N = round(Int, T/dt + 1) 
+N = round(Int, T/dt + 1) # number of data point 
 
 t = dt .* collect(0:N-1) # Time [s]
 
@@ -64,8 +64,43 @@ p2 = plot(t, u2, line=(:blue, 1, :solid),
     ylim = (-1.5, 1.5)
     )
 
-plot(p1, p2, layout = (2,1), legend=false)
+#plot(p1, p2, layout = (2,1), legend=false)
 
 #savefig("test.png")
 
 #Do cross-correlation in time domain
+#add zero for the length of N-1 to both sides of u2
+#normalized by the number of data points N-k (k is the data number in zero padding)
+
+Rcorr	= zeros(Float64, 2*N-1)
+u2corr	= vcat(zeros(N-1), u2, zeros(N-1)) 
+k 		= vcat(collect(N-1:-1:0), collect(1:N-1))
+
+for n = 1:2*N-1
+
+	#scale = 1/(N-k[n]) #scale by 1/(N-k)
+	scale = 1/N
+
+	for m = 1:N
+
+		Rcorr[n] += scale * u1[m] * u2corr[n+m-1]
+
+	end
+end
+
+#time axis is determined by the sampling rate
+tcorr = dt .* collect(-(N-1):(N-1))
+
+p3 = plot(tcorr, Rcorr, line=(:red, 1, :solid),
+	marker = (:cross, 2, :green),
+    ylabel = "Correlation", 
+    xlabel = "Time [s]",
+    title = "Cross-correlation between u1 and u2",
+    xlim = (-T, T),
+    ylim = (-1.2*maximum(abs.(Rcorr)), 1.2*maximum(abs.(Rcorr)))
+    )
+
+plot(p1, p2, p3, layout = (3,1), 
+				 size = (800, 1200),
+				 legend=false)
+
